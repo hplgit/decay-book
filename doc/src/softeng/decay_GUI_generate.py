2d6fc2904e012ad8af13839b5288af5d0d985038
Hans Petter Langtangen <hpl@simula.no>
2015-10-18 02:45:50 -0700

    updates

--- doc/src/softeng/decay_GUI_generate.py
+++
@@ -1,6 +0,0 @@
-from parampool.generator.flask import generate
-from decay import main_GUI
-generate(main_GUI,
-         filename_controller='decay_GUI_controller.py',
-         filename_template='decay_GUI_view.py',
-         filename_model='decay_GUI_model.py')
